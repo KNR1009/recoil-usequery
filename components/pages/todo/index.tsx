20fdc19dbb148ac8f53cdc275a53e9dc1f71a98e
伊東和成 <itokazunari5375@icloud.com>
2022-02-12 15:02:46 +0900

    簡易的な削除ボタンの作成

--- components/pages/todo/index.tsx
+++ components/pages/todo/index.tsx
@@ -6,6 +6,9 @@ import { Task } from "../../../interfaces/Task";
 // import { Sample1Memo } from "../../sample/sample1";
 // import { Sample2Memo } from "../../sample/sample2";
 
+// styled
+import styled from "styled-components";
+
 type TodoProps = {
   tasks: Task[];
 };
@@ -18,17 +21,50 @@ export const Todo: React.VFC<TodoProps> = ({ tasks }) => {
   useEffect(() => {
     console.log("recoilデータを更新");
     setTasksLists(tasks);
+    // TasksListsが変わった場合のみ更新 => データが変更しない限りレンダリングは発生しない
   }, []);
 
   console.log("todoListコンポーネントのレンダリング");
 
-  // recoilのstateを変更
+  // 削除コール
+  const onDelete = (id: number) => {
+    // 削除用のapiコールをしてデータを更新
+    console.log(id);
+  };
 
+  // 今現在はuseQueryの値を直接代入している
   return (
     <div>
       <div>Recoilのstateを更新</div>
+      {TasksLists.map((task, index) => (
+        <Task key={index}>
+          <p>タスク名: {task.title} </p>
+          <p
+            className="task-delete"
+            onClick={() => {
+              onDelete(task.id);
+            }}
+          >
+            削除
+          </p>
+        </Task>
+      ))}
       {/* <Sample1Memo />
       <Sample2Memo /> */}
     </div>
   );
 };
+
+const Task = styled.div`
+  display: flex;
+  align-items: center;
+
+  .task-delete {
+    background-color: red;
+    color: #ffffff;
+    padding: 5px;
+    margin-left: 10px;
+    border-radius: 10px;
+    cursor: pointer;
+  }
+`;
